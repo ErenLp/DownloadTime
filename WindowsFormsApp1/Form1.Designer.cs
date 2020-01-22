2ece47de338f93618c042d2a7c63c444d874db4c
ErenLp <60190930+ErenLp@users.noreply.github.com>
2020-01-22 22:27:34 +0300

    Commited

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Beige;
+            this.button1.Location = new System.Drawing.Point(240, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(451, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.calculate);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Candara", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GB",
+            "MB"});
+            this.comboBox1.Location = new System.Drawing.Point(448, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(64, 37);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "MB";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Carlito", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(90, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(328, 37);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Enter File Size";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyPressed);
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textPressed);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(596, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 36);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0.85";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.text2Pressed);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(0, 223);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(839, 37);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.moana_16spp_oidn1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(839, 260);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.LightCoral;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download Time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyPressed(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+                button1.PerformClick();
+        }
+        static int x = 0,y=0;
+        private void textPressed(object sender, MouseEventArgs e)
+        {
+            if (x++ == 0)
+            {
+                switch (MouseButtons)
+                {
+                    case MouseButtons.Left:
+                        textBox1.Text = "";
+                        textBox1.ForeColor = Color.Black;
+                        break;
+                }
+            }
+        }
+        private void text2Pressed(object sender, MouseEventArgs e)
+        {
+            if (y++ == 0)
+            {
+                switch (MouseButtons)
+                {
+                    case MouseButtons.Left:
+                        textBox2.Text = "";
+                        textBox2.ForeColor = Color.Black;
+                        break;
+                }
+            }
+        }
+        private void calculate(object sender, EventArgs e)
+        {
+            double value, speed;
+            if (comboBox1.Text == "MB")
+            {
+                value = Double.Parse(textBox1.Text);
+                speed = Double.Parse(textBox2.Text);
+            }
+            else
+            {
+                value = Double.Parse(textBox1.Text)*1000;
+                speed = Double.Parse(textBox2.Text);
+            }
+                int seconds = Convert.ToInt32(value / speed);
+                int mins = seconds / 60;
+                int hours = mins / 60;
+                int days = hours / 24;
+                int[] ar = { hours, mins, seconds ,days};
+                int count = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    if (ar[i] == 0)
+                        count++;
+                }
+                switch (count)
+                {
+                    case 0:
+                        hours = hours - days*24; 
+                        mins =mins- hours * 60-days*1440;
+                        seconds =seconds- mins * 60-hours*3600-days*86400;
+                        label1.Text =days.ToString()+" days "+ hours.ToString() + " hours " + mins.ToString() + " minutes " + seconds.ToString() + " seconds.";
+                        break;
+                    case 1:
+                        mins -= hours * 60;
+                        seconds =seconds- mins * 60  - hours * 3600;
+                        label1.Text = hours.ToString()+" hours "+mins.ToString() + " minutes " + seconds.ToString() + " seconds. ";
+                        break;
+                    case 2:
+                        seconds = seconds - mins * 60  - hours * 3600;
+                        label1.Text =mins.ToString()+" minutes "+ seconds.ToString() + " seconds. ";
+                        break;
+                    case 3:
+                        label1.Text = seconds.ToString() + " seconds. ";
+                        break;
+                }
+            
+        }
+
+    }
+}
